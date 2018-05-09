0841af079b74fb918dac4ef050f8201b740005a5
fernanda <fernandakagami@gmail.com>
2018-05-09 14:14:06 -0300

    Building sessions - logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+
   def new
   end
 
@@ -14,5 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out
+    redirect_to root_url
   end
 end
